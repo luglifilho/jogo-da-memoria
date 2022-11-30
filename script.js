25c8a7cf42a241a6817aff967a9a0f717f948d0c
luglifilho <luglifilho@hotmail.com>
2022-11-29 21:01:54 -0300

    function_cards

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const cards = document.querySelectorAll('.card');
 let hasflippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard(){
+    if(lockBoard)return;
+    if(this === firstCard) return;
     this.classList.add('flip');
     if(!hasflippedCard){
         hasflippedCard = true;
@@ -10,8 +13,35 @@ function flipCard(){
         return;
     }
     secondCard = this;
+    hasflippedCard = false
     checkCards();
 }
+
+function checkCards(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unflipCard();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCard(){
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    }, 1500);
+}
+
+
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
